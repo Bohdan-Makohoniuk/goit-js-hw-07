7b366a695a875aa45844b2c46868472ca459e989
Bohdan <makohoniuk.bo@gmail.com>
2022-09-03 14:15:59 +0300

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,32 +13,100 @@
 // instance.show()
 
 
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+
+// console.log(galleryItems);
+
+
+
+// createImageCardsMarkUp(cardImage);
+
+// function createImageCardsMarkUp(cardImage) {
+//   return galleryItems.map(({ preview, original, description }) => {
+//     return `
+//     <div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"/>
+//   </a>
+// </div>
+//     `;
+//   }).join('');
+  
+// }
+
+
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
 
+galleryContainer.innerHTML = creatElements(galleryItems);
 
-
-createImageCardsMarkUp(cardImage);
-
-function createImageCardsMarkUp(cardImage) {
-  return galleryItems.map(({ preview, original, description }) => {
-    return `
-    <div class="gallery__item">
-  <a class="gallery__link" href="${original}">
+function creatElements(gallery) {
+  const imageEl = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
     <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"/>
-  </a>
-</div>
-    `;
-  }).join('');
-  
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </div>`
+    )
+    .join('');
+
+  return imageEl;
 }
+
+let modalWindow = null;
+
+
+  const showModalLightbox = ({ alt, dataset: { source } }) => {
+    modalWindow = basicLightbox.create(
+      `<img style="color: #fff" src="${source}" alt="${alt}" width="800" height="600">`,
+      {
+        onShow: addKeyboardControl,
+        onClose: removeKeyboardControl,
+      }
+    );
+
+    modalWindow.show();
+  
+
+  const addKeyboardControl = () => window.addEventListener('keydown', onWindowKeyDown);
+  const removeKeyboardControl = () => window.removeEventListener('keydown', onWindowKeyDown);
+
+  const onWindowKeyDown = ({ code }) => {
+    if (code !== 'Escape') return;
+
+    modalWindow.close();
+  };
+
+  const onGalleryContainerClick = event => {
+    event.preventDefault();
+
+    if (event.target.nodeName !== 'IMG') return;
+
+    showModalLightbox(event.target);
+  };
+
+  galleryContainer.addEventListener('click', onGalleryContainerClick);
+}
+
+
 
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = creatElements(galleryItems);
+
+function creatElements(gallery) {
+  const imageEl = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+</li>`
+    )
+    .join('');
+
+  return imageEl;
+}
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, navText: ['←','→'] });
+
 console.log(galleryItems);
